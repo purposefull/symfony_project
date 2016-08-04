9c929ba7121e902d54dbf45f70a9d5fe979c616b
maxim <o.yakubenko2014@yandex.ua>
2016-08-04 17:01:10 +0300

    entity

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,3 +19,61 @@ class DefaultController extends Controller
         ]);
     }
 }
+
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
+
+// ...
+public function createAction()
+{
+    $product = new Product();
+    $product->setName('Keyboard');
+    $product->setPrice(19.99);
+    $product->setDescription('Ergonomic and stylish!');
+
+    $em = $this->getDoctrine()->getManager();
+
+    // tells Doctrine you want to (eventually) save the Product (no queries yet)
+    $em ->persist($product);
+
+    // actually executes the queries (i.e. the INSERT query)
+    $em->flush();
+
+    return new Response('Saved new product with id '.$product->getId());
+}
+
+public function showAction($productId)
+{
+    $product = $this->getDoctrine()
+        ->getRepository('AppBundle:Product')
+        ->find($productId);
+
+    if (!$product) {
+        throw $this->createNotFoundException(
+            'No product found for id '.$productId
+        );
+    }
+
+    // ... do something, like pass the $product object into a template
+}
+
+public function updateAction($productId)
+{
+    $em = $this->getDoctrine()->getManager();
+    $product = $em->getRepository('AppBundle:Product')->find($productId);
+
+    if (!$product) {
+        throw $this->createNotFoundException(
+            'No product found for id '.$productId
+        );
+    }
+
+    $product->setName('New product name!');
+    $em->flush();
+
+    return $this->redirectToRoute('homepage');
+
+}
+
+
+
---
+++ src/AppBundle/Entity/Entity.php~
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entity
+ *
+ * @ORM\Table(name="entity")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EntityRepository")
+ */
+class Entity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
